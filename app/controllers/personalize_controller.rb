26e64c04633670070b0b7386079c11049347ffc9
Stephen Huey <me@stephenhuey.com>
2017-08-30 21:47:39 -0500

    Retrieve items from the API and try laying them out with in_groups_of helper method.

--- app/controllers/personalize_controller.rb
+++ app/controllers/personalize_controller.rb
@@ -1,4 +1,7 @@
 class PersonalizeController < ApplicationController
   def index
+    response = Faraday.get 'https://api.tintup.com/v1/feed/hueydemo?api_token=05511baf4de385e6582942fa38aa3928b3bfadc8'
+    json = JSON.parse response.body
+    @items = json["data"]
   end
 end
