3e4012cb8c9e2b46fbb29dcb3caa7184f64ce470
Stephen Huey <me@stephenhuey.com>
2017-08-31 07:51:09 -0500

    Sound the alarm if no api token is provided.

--- config/initializers/tint.rb
+++ config/initializers/tint.rb
@@ -1,4 +1,4 @@
-TINT_API_TOKEN = ENV['TINT_API_TOKEN']
+TINT_API_TOKEN = ENV['TINT_API_TOKEN'] || raise('No TINT_API_TOKEN provided to the environment!')
 
 ::TINT_API_URL = "https://api.tintup.com/v1/feed/hueydemo?api_token=#{TINT_API_TOKEN}&source=instagram"
 
