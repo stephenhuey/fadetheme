443348a6607c7f826145e577fd2f9fcc7ab49143
Stephen Huey <me@stephenhuey.com>
2017-08-31 08:13:57 -0500

    Make sure you set your config var on Heroku

--- config/initializers/tint.rb
+++ config/initializers/tint.rb
@@ -1,3 +1,5 @@
+# Remember to set the config var on Heroku!
+
 TINT_API_TOKEN = ENV['TINT_API_TOKEN'] || raise('No TINT_API_TOKEN provided to the environment!')
 
 ::TINT_API_URL = "https://api.tintup.com/v1/feed/hueydemo?api_token=#{TINT_API_TOKEN}&source=instagram"
